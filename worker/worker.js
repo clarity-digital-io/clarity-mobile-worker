3c76986d89ae9db7b203b7579b609f1ed01df847
Ben Efigenio <befigenio@gmail.com>
2020-06-22 20:16:12 -0500

    test

--- worker/worker.js
+++ worker/worker.js
@@ -1,9 +1,9 @@
 import throng from 'throng';
 import Queue from 'bull';
 
-let PORT = '10579';
-let HOST = 'ec2-54-205-115-98.compute-1.amazonaws.com';
-let PASSWORD = 'p3b2f0b8e50eb4fca2646a65350018b26f04d1cbb8074c2a20475936bec4b1da1';
+let PORT = '19499';
+let HOST = 'ec2-52-202-160-22.compute-1.amazonaws.com';
+let PASSWORD = 'p2be04e53cb71f4970daa5e90bc1f15f0c2086fd2850609eef7c057babf2051aa';
 
 let workers = process.env.WEB_CONCURRENCY || 2;
 
@@ -22,18 +22,6 @@ function start() {
   workQueue.process(maxJobsPerWorker, async (job) => {
 		console.log('workqueue', job); 
     let progress = 0;
-
-    // throw an error 5% of the time
-    if (Math.random() < 0.05) {
-      throw new Error("This job failed!")
-    }
-
-    while (progress < 100) {
-      await sleep(50);
-      progress += 1;
-      job.progress(progress)
-    }
-
     // A job can return values that will be stored in Redis as JSON
     // This return value is unused in this demo application.
     return { value: "This will be stored" };
