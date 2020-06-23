5750eb418c6a793c90405a3fa764bd60776e5082
Ben Efigenio <befigenio@gmail.com>
2020-06-23 12:12:31 -0500

    test

--- worker/schema/index.js
+++ worker/schema/index.js
@@ -5,7 +5,7 @@ export const FormSchema = {
     Id: 'string',
 		Name: 'string',
 		Title__c: 'string',
-		Description__c: 'data?',
+		Description__c: 'string',
 		Status__c: 'string',
 		Multi_Page__c: {type: 'bool', default: false},
 		Multi_Page_Val__c: {type: 'bool', default: false},
