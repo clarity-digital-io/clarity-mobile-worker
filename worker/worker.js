c1ceb27cb8a8dcfbaadcd3660d74efffe7083840
Ben Efigenio <befigenio@gmail.com>
2020-06-22 20:04:39 -0500

    test

--- worker/worker.js
+++ worker/worker.js
@@ -18,7 +18,7 @@ function start() {
 	let workQueue = new Queue('connect', {redis: {port: PORT, host: HOST, password: PASSWORD }}); 
 
   workQueue.process(maxJobsPerWorker, async (job) => {
-
+		console.log('workqueue', job); 
     let progress = 0;
 
     // throw an error 5% of the time
