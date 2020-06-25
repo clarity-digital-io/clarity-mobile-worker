f0944c5149a33975f94955191f52db28a6c42e04
Ben Efigenio <befigenio@gmail.com>
2020-06-25 13:54:38 -0500

    test

--- worker/consumer/answers.js
+++ worker/consumer/answers.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+const grant_type = 'password';     
+const client_id = '3MVG9Z8h6Bxz0zc4V._snL15FoFtwlrYRmvezul8wMJk0jx5CqffMMlS0afWQIQ9clkd1mZOxy.j6DTR4p7m9'; 
+const client_secret = '00A082693387D6718AC18AEA088DC459F656E945B96FFE219EBF40C2D9345136'; 
+const username = 'test-is7noj24esie@example.com';
+const password = 'Clarity2020!hK0S8pi2pXOQ7tjsADGijhFV';
+
 export const sendAnswers = async (job, done) => {
 	
 		const answers = job.data;
