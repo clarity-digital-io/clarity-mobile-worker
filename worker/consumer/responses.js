4a81d7deb1acc8d0682b3de40253dcbd4b9de3d1
Ben Efigenio <befigenio@gmail.com>
2020-06-27 11:30:40 -0500

    test

--- worker/consumer/responses.js
+++ worker/consumer/responses.js
@@ -69,12 +69,7 @@ const deleteResponseByIds = async ({instance_url, access_token, refresh_token}, 
 	console.log('responseUUIDs', instance_url, access_token, responseUUIDs); 
 
 	try {
-		const response = axios.create({
-			baseURL: `${instance_url}/services/apexrest/forms/v1/Responses`,
-			method: 'delete',
-			headers: { Authorization: "Bearer " + access_token },
-			data: { data: responseUUIDs }
-		});
+		const response = await axios.delete(`${instance_url}/services/apexrest/forms/v1/Responses`, { data: responseUUIDs, 	headers: { Authorization: "Bearer " + access_token } });
 		return response; 
 	} catch (error) {
 		if(error.response) {
