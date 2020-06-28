f0136a09bd64a35f1b1bb71d7deb75a02d39c75c
Ben Efigenio <befigenio@gmail.com>
2020-06-28 12:02:13 -0500

    update schema

--- worker/consumer/answers.js
+++ worker/consumer/answers.js
@@ -3,23 +3,38 @@ import { getClientAccessToken } from '../helpers/access';
 
 export const sendAnswers = async (job, done) => {
 	
-		const answers = job.data.body;
+		const body = job.data.body;
 		const organizationId = job.data.organizationId;
 
-		const response = await sync(answers, organizationId);
+		const response = await sync(body, organizationId);
 
 		done(null, { organizationId: organizationId });
 		
 }
 
-const sync = async (answers, organizationId) => {
+const sync = async (body, organizationId) => {
 
 	const data = await getClientAccessToken(organizationId);
+
+	const [answers, attachments] = filterAnswers(JSON.parse(body)); 
 	
-	const response = await updateAnswers(data, answers); 
+	if(answers.length > 0) {
+		const response = await updateAnswers(data, answers); 
+		return response; 
+	}
 
-	return response; 
+	if(attachments.length > 0) {
+		const responseAttachments = await updateAttachments(data, attachments); 
+		return responseAttachments; 
+	}
 
+}
+
+const filterAnswers = (body) => {
+
+	let answers = body.filter(ans => !ans.IsActive);
+	let attachments = body.filter(ans => ans.IsActive);
+	return [answers, attachments];
 }
 
 const updateAnswers = async ({instance_url, access_token}, answers) => {
@@ -38,5 +53,24 @@ const updateAnswers = async ({instance_url, access_token}, answers) => {
 			console.log('data', data); 
 		}
 	}
+
+}
+
+const updateAttachments = async ({instance_url, access_token}, attachments) => {
+	console.log('attachments', attachments); 
+	// try {
+	// 	const response = await axios.post(`${instance_url}/services/apexrest/forms/v1/Attachments`, { data: attachments }, { headers: { Authorization: "Bearer " + access_token } });
+	// 	return response; 
+	// } catch (error) {
+	// 	if(error.response) {
+	// 		const { data } = error.response; 
+	// 		console.log('data', data); 
+	// 	}
+
+	// 	if(error.request) {
+	// 		const { data } = error.request; 
+	// 		console.log('data', data); 
+	// 	}
+	// }
 
 }
