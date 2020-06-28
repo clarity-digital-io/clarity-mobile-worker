650dc1065cdad0390e351b0a962ad73d91822888
Ben Efigenio <befigenio@gmail.com>
2020-06-28 13:26:48 -0500

    update schema

--- worker/consumer/answers.js
+++ worker/consumer/answers.js
@@ -59,7 +59,7 @@ const updateAnswers = async ({instance_url, access_token}, answers) => {
 const updateAttachments = async ({instance_url, access_token}, attachments) => {
 	console.log('attachments', attachments); 
 	try {
-		const response = await axios.post(`${instance_url}/services/apexrest/forms/v1/Attachments`, { data: attachments }, { headers: { Authorization: "Bearer " + access_token } });
+		const response = await axios.post(`${instance_url}/services/apexrest/forms/v1/Attachments`, { data: JSON.stringify(attachments) }, { headers: { Authorization: "Bearer " + access_token } });
 		return response; 
 	} catch (error) {
 		if(error.response) {
