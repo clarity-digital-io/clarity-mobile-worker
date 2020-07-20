2acaa64ce25a4404a652fdb7f9965ad374128bfe
Ben Efigenio <befigenio@gmail.com>
2020-07-20 17:34:22 -0500

    test

--- worker/consumer/connect.js
+++ worker/consumer/connect.js
@@ -5,16 +5,16 @@ import { sync } from '../realm/sync';
 export const connect = async (job, done) => {
 	console.log('job',job);
 
-		const realm = await openRealm(job.data.organizationId);
-	
-		const forms = prepare(job.data.forms); 
+	const realm = await openRealm(job.data.organizationId);
 
-		const groups = prepareGroups(job.data.groups); 
-		console.log('groups',groups);
-		sync(realm, forms, groups);
+	const forms = prepare(job.data.forms); 
 
-		realm.close(); 
+	const groups = prepareGroups(job.data.groups); 
+	console.log('groups',groups);
+	sync(realm, forms, groups);
 
-		done(null, { organizationId: job.data.organizationId });
+	realm.close(); 
+
+	done(null, { organizationId: job.data.organizationId });
 		
 }
