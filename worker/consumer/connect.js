ce4b3c50d096b12f333fde6804014747b85eec24
Ben Efigenio <befigenio@gmail.com>
2020-07-20 19:18:38 -0500

    test

--- worker/consumer/connect.js
+++ worker/consumer/connect.js
@@ -9,6 +9,7 @@ export const connect = async (job, done) => {
 		const realm = await openRealm(job.data.organizationId);
 	
 		const forms = prepare(job.data.forms); 
+		console.log('forms',forms);
 
 		const groups = prepareGroups(job.data.groups); 
 		console.log('groups',groups);
