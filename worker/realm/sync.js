d19a4154573947f4c37c3f845b1f0630a4411873
Ben Efigenio <befigenio@gmail.com>
2020-08-13 11:51:04 -0500

    test

--- worker/realm/sync.js
+++ worker/realm/sync.js
@@ -30,7 +30,7 @@ export const sync = (realm, forms, groups, syncForm) => {
 
 			//need to get only questions with the form we are updating with it's a form sync
 			let newQuestions = realm.objects('Question').filtered('Form == $0', preparedForm.Id); //can query for the ones with options here
-
+			console.log('newQuestions', newQuestions); 
 			newQuestions.forEach(question => {
 
 				let questionOptionsList = question.Question_Options;
